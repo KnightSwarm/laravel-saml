5a8c2a2f901772543752dc7de99f3ef3795aeb47
Diego Hernandes <diego@he.rnand.es>
2014-03-20 00:08:44 -0300

    working simplesaml loading

--- src/KnightSwarm/LaravelSaml/Saml/Boot.php
+++ src/KnightSwarm/LaravelSaml/Saml/Boot.php
@@ -11,18 +11,19 @@ class Boot {
 
     public function __construct()
     {
-        $this->path = Config::get('package::saml.sp_path', public_path()."/sp1");
-        $this->sp   = Config::get('package::saml.sp_name', 'default_sp');
+        $this->path = Config::get('package::saml.sp_path', public_path()."/sp/www");
+        $this->sp   = Config::get('package::saml.sp_name', 'default-sp');
 
         require_once($this->path.'/lib/_autoload.php');
 
-        $this->saml = new SimpleSAML_Auth_Simple($this->sp);
+        $this->saml = new \SimpleSAML_Auth_Simple($this->sp);
 
 
     }
 
     public function getAttributes()
     {
-        return $this->saml->getAttributes();
+
+        return $this->saml->getLogoutURL();
     }
 } 
