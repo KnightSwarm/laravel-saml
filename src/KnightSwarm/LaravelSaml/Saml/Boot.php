bd256913c68097518beef1adfb4d4bdca59082ec
Diego Hernandes <diego@he.rnand.es>
2014-03-19 23:40:29 -0300

    testing it again

--- src/KnightSwarm/LaravelSaml/Saml/Boot.php
+++ src/KnightSwarm/LaravelSaml/Saml/Boot.php
@@ -4,6 +4,24 @@
 class Boot {
 
 
-    private $path;
-    private 
+    protected $path;
+    protected $sp;
+    protected $saml;
+
+    public function __construct()
+    {
+        $this->path = Config::get('package::saml.sp_path', public_path()."/sp1");
+        $this->sp   = Config::get('package::saml.sp_name', 'default_sp');
+
+        require_once($this->path.'/lib/_autoload.php');
+
+        $this->saml = new SimpleSAML_Auth_Simple($this->sp);
+
+
+    }
+
+    public function getAttributes()
+    {
+        return $this->saml->getAttributes();
+    }
 } 
