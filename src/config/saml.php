3c8a16e339f9030e9ff04e36f58cbd7fbc9a26eb
Jeremy French <jeremy@jeremyfrench.co.uk>
2014-08-29 17:12:40 +0100

    update settings file with documentation of new features.

--- src/config/saml.php
+++ src/config/saml.php
@@ -27,4 +27,38 @@ return array(
      * The redirect destination after logging out
      */
     'logout_target' => 'http://saml.dev',
+
+    /*
+     * Internal id property, defaults to email.
+     * The property to identify users by in the system.
+     * 'internal_id_property' => 'email',
+     */
+
+    /*
+     * Saml id property defaults to email, the property
+     * in the saml packet which should be mapped to the
+     * internal id property.
+     * 'saml_id_property' => 'email',
+     */
+
+    /*
+     * object_mappings.
+     * an array of string with string keys.
+     * Key is a value on your User object, value is the
+     * SAML value that this is mapped to.
+     * When user is created these values will be copied
+     * to your user object.
+     *
+     * 'object_mappings' => [
+     *  'email' => 'email',
+     * ],
+     */
+
+    /*
+     * can_create and can_create_error.
+     * If you don't want users to be created via saml set can_create to false.
+     * They will then be presented with an error and the value of can_create_error.
+     * 'can_create' => false,
+     * 'can_create_error' => 'You can not register on this system.',
+     */
 );
