60b0fefca6b48a7804e20bcfe879cceee20f5cbb
Jeremy French <jeremy@jeremyfrench.co.uk>
2014-09-23 16:04:28 +0100

    Fix binding issues.

--- src/KnightSwarm/LaravelSaml/SamlSpResolver.php
+++ src/KnightSwarm/LaravelSaml/SamlSpResolver.php
@@ -1,6 +1,10 @@
-<?php
+<?php namespace KnightSwarm\LaravelSaml;
 
 class SamlSpResolver {
+  public function __construct($app) {
+    $this->app = $app;
+  }
+
   public function getSPName() {
     return $this->app->config->get('laravel-saml::saml.sp_name', 'default-sp');
   }
